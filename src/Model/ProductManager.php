af79b267cc757c891ce8837092f3b5a9b62e6dce
sdesousa <sylvaindesousa@free.fr>
2019-11-02 20:17:54 +0100

    S01US26 creation ProductManager

---
+++ src/Model/ProductManager.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Model;
+
+class ProductManager extends AbstractManager
+{
+    const TABLE = 'product';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+}
