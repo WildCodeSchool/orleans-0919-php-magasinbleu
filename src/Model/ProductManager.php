40cd9fa6a98eedc50f6d378c0111e11d4a0c5a8d
Nolwenn <sachet.nolwenn@orange.fr>
2019-11-05 18:13:19 +0100

    affichage des marques et catégories en twig

--- src/Model/ProductManager.php
+++ src/Model/ProductManager.php
@@ -6,6 +6,8 @@ class ProductManager extends AbstractManager
 {
     const TABLE = 'product';
     const TABLE_UNIVERSE = 'universe';
+    const TABLE_CATEGORY = 'category';
+    const TABLE_BRAND = 'brand';
 
     /**
      *  Initializes this class.
@@ -15,10 +17,14 @@ class ProductManager extends AbstractManager
         parent::__construct(self::TABLE);
     }
 
-    /* 3 last products Homepage */
+    /* 3 last products Hmepage */
     public function selectAll(): array
     {
-        return $this->pdo->query('SELECT * FROM ' . $this->table . ' ORDER BY id DESC LIMIT 3 ')->fetchAll();
+        $query = 'SELECT p.*, c.name AS category_name, b.name AS brand_name FROM ' . $this->table .
+                    ' p JOIN ' . self::TABLE_CATEGORY . ' c ON p.category_id = c.id 
+                        JOIN ' . self::TABLE_BRAND . ' b ON p.brand_id = b.id 
+                        ORDER BY p.id DESC LIMIT 3 ';
+        return $this->pdo->query($query)->fetchAll();
     }
 
     /**
