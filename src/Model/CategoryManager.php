6daaf185d22065045e9aa89086a42acbb9bebe99
Dimitri Gille <dimitri45220@hotmail.com>
2019-11-14 19:03:05 +0100

    mise en place de l'update category

--- src/Model/CategoryManager.php
+++ src/Model/CategoryManager.php
@@ -24,4 +24,15 @@ class CategoryManager extends AbstractManager
         $categories = $statement->fetchAll();
         return $categories;
     }
+
+    public function update(array $data)
+    {
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . "
+                SET name=:name            
+                WHERE id=:id
+            ");
+        $statement->bindValue('name', $data['name'], \PDO::PARAM_STR);
+        $statement->bindValue('id', $data['id'], \PDO::PARAM_INT);
+        $statement->execute();
+    }
 }
