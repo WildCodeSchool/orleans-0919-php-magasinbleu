bae44568f0f61252164c230d908dd3deb25dd379
Lionel Delamare <lionel.delamare@hotmail.com>
2019-11-14 17:31:18 +0100

    Corrections formulaire contact client - Lionel

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -7,22 +7,15 @@ use App\Model\AbstractManager;
 class ContactController extends AbstractController
 {
 
-    public function cleanInput(array $data): array
-    {
-        foreach ($data as $key => $value) {
-            $data[$key] = trim($value);
-        }
-        return $data;
-    }
-
     public function contact()
     {
         $name = $email = $phone = $message = '';
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $data = $this->cleanInput($_POST);
+            //$data = $this->cleanInput($_POST);
+            $data = array_map('trim', $_POST);
             $errors = [];
 
-            if (strlen($data['name']) > 35) {
+            if (strlen($data['name']) > 50) {
                 $errors['name'][] = 'Le nom est trop long';
             }
             if (empty($data['name'])) {
@@ -39,12 +32,6 @@ class ContactController extends AbstractController
                 $email = $data['email'];
             }
 
-            if (empty($data['phone'])) {
-                $errors['phone'][] = "Veuillez indiquer un numéro de téléphone";
-            } else {
-                $phone = $data['phone'];
-            }
-
             if (empty($data['message'])) {
                 $errors['message'][] = "Veuillez indiquer votre message";
             } else {
@@ -52,10 +39,10 @@ class ContactController extends AbstractController
             }
 
             if (empty($errors)) {
-                $name = htmlentities($data['name']);
-                $email = htmlentities($data['email']);
-                $phone = htmlentities($data['phone']);
-                $message = htmlentities($data['message']);
+                $name = $data['name'];
+                $email = $data['email'];
+                $phone = $data['phone'];
+                $message = $data['message'];
                 header('location: /Contact/contact');
             }
         }
