1ab8ab545237cdde28763235e08cd278b1fbe51d
Romain PETIT <rom.petit@live.fr>
2019-11-19 14:49:37 +0100

    deplacement de la partie contact

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,6 +13,49 @@ class HomeController extends AbstractController
         $universes = $universeManager->selectAll();
         $productManager = new ProductManager();
         $products = $productManager->lastProduct();
-        return $this->twig->render('Home/index.html.twig', ['universes' => $universes, 'products' => $products]);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = array_map('trim', $_POST);
+            $errors = $this->validate($data);
+
+            if (empty($errors)) {
+                // TODO : envoi du mail
+                header('location: /Home/index/?success=ok');
+            }
+        }
+
+        return $this->twig->render('Home/index.html.twig', [
+            'errors' => $errors ?? [],
+            'contact' => $data ?? [],
+            'universes' => $universes,
+            'products' => $products,
+            ]);
     }
+
+    private function validate(array $data) :array
+    {
+        if (strlen($data['name']) > 50) {
+            $errors['name'][] = 'Le nom est trop long';
+        }
+        if (empty($data['name'])) {
+            $errors['name'][] = "Veuillez indiquer votre nom";
+        }
+
+        if (empty($data['email'])) {
+            $errors['email'][] = "Veuillez indiquer une adresse mail";
+        } elseif (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors['email'][] = "Format invalide";
+        }
+
+        if (empty($data['subject'])) {
+            $errors['subject'][] = "Veuillez indiquer l'objet de votre message";
+        }
+
+        if (empty($data['message'])) {
+            $errors['message'][] = "Veuillez indiquer votre message";
+        }
+
+        return $errors ?? [];
+    }
+
 }
